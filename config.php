6b1b08ba16616f7f4416fa2f089bdb2e949f1586
gonxaok <64032319+gonxaok@users.noreply.github.com>
2020-04-20 14:13:09 -0300

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 session_start();
 
 
-mysql_connect('localhost', 'id13362381_test1', 'OPtWKTBl)s7*D7{=');
-mysql_select_db('id13362381_test');
+mysql_connect('localhost', 'user', 'password');
+mysql_select_db('dbname');
 
 
 $mail_webmaster = '';
